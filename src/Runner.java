92abaeaabefc6234f76dcd93e1c12438501a0811
hkaur2001 <36794921+hkaur2001@users.noreply.github.com>
2019-01-15 05:30:42 -0500

    Update Runner.java

--- src/Runner.java
+++ src/Runner.java
@@ -1,2 +1,11 @@
 public class Runner {
+
+    public static void main(String[] args) {
+
+        String [] randomStringArray = inPlaceSorts.randomStringArr(10);
+                System.out.println("Before: ");
+        System.out.println(inPlaceSorts.printArray(randomStringArray));
+
+
+    }
 }
