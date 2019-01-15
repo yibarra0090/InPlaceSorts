0870ba0e89fe878f1c8e4521ed378150cb1f43d5
BT_1N3_27 <yibarra0090@bths.edu>
2019-01-15 15:43:53 -0500

    done!

--- src/InPlaceSorts.java
+++ src/InPlaceSorts.java
@@ -1,6 +1,6 @@
 public class InPlaceSorts {
     //SORTING ALG
-    public static void sortStringBubble( String [] arr) {
+    public static void bubbleSort(String[] arr) {
         int i;
         boolean check = true;  // will determine when the sort is finished
         String temp;
@@ -15,13 +15,15 @@ public class InPlaceSorts {
                     check = true;
                 }
             }
-        }}
-    public static void selectionSort(double [] arr){
+        }
+    }
+
+    public static void selectionSort(double[] arr) {
         int minPos = 0;
-        for (int curPos = 0; curPos < arr.length; curPos++){
+        for (int curPos = 0; curPos < arr.length; curPos++) {
             double minVal = arr[curPos];
-            for(int nextPos = curPos +1; nextPos < arr.length; nextPos++){
-                if (arr[nextPos]< minVal){
+            for (int nextPos = curPos + 1; nextPos < arr.length; nextPos++) {
+                if (arr[nextPos] < minVal) {
                     minVal = arr[nextPos];
                     minPos = nextPos;
                 }
@@ -29,12 +31,13 @@ public class InPlaceSorts {
             }
         }
     }
-    public static void InsertionSort(int [] arr){
-        for (int i = 0; i<arr.length;i++){
-            for(int j = i; j>1; j--){
-                if (arr[j]<arr[j-1]){
-                    swap(arr,i,j-1);}
-                else {
+
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    swap(arr, j, j - 1);
+                } else {
                     break;
                 }
             }
@@ -42,46 +45,49 @@ public class InPlaceSorts {
     }
 
     //SWAP
-    public static void swap(int[]arr,int a, int b){
+    public static void swap(int[] arr, int a, int b) {
         int temp = arr[a];
         arr[a] = arr[b];
         arr[b] = temp;
     }
 
-    public static void swapdouble(double[]arr,int a, int b) {
+    public static void swapdouble(double[] arr, int a, int b) {
         double temp = arr[a];
         arr[a] = arr[b];
         arr[b] = temp;
     }
-    public static void swapString(String[]arr,int a, int b) {
+
+    public static void swapString(String[] arr, int a, int b) {
         String temp = arr[a];
         arr[a] = arr[b];
         arr[b] = temp;
     }
 
     //Random Generators
-    public static int[] randIntArr(int count){
+    public static int[] randIntArr(int count) {
         int[] num = new int[count];
-        for(int i = 0; i < count; i++){
-            num[i] = (int)(Math.random()*10001);
+        for (int i = 0; i < count; i++) {
+            num[i] = (int) (Math.random() * 10001);
         }
         return num;
     }
-    public static double[] randDoubleArr(int count){
+
+    public static double[] randDoubleArr(int count) {
         double[] num = new double[count];
-        for(int i =0; i < count; i++){
-            num[i] = (Math.random()*10000);
+        for (int i = 0; i < count; i++) {
+            num[i] = (Math.random() * 10000);
         }
-        return  num;
+        return num;
     }
-    public String [] randomStringArr(int num, int length){
-        String [] arr = new String [num];
-        while (num > 0){
-            int i =0;
+
+    public static String[] randomStringArr(int num, int length) {
+        String[] arr = new String[num];
+        while (num > 0) {
+            int i = 0;
             String s = "";
-            while (i < length){
-                char c = (char)((Math.random()*26)+97);
-                s = s+ c;
+            while (i < length) {
+                char c = (char) ((Math.random() * 26) + 97);
+                s = s + c;
                 i++;
             }
             num--;
@@ -91,43 +97,80 @@ public class InPlaceSorts {
     }
 
     //Copy
-     public static int[] copyIntArr(int[] arr){
-        int [] copyArr = new int[arr.length];
-        for (int i = 0; i < arr.length; i++){
-            copyArr[i] = arr[i];
-        }
-        return copyArr;
-     }
-    public static double[] copyDoubleArr(double[] arr){
-        double [] copyArr = new double[arr.length];
-        for (int i = 0; i < arr.length; i++){
+    public static int[] copyIntArr(int[] arr) {
+        int[] copyArr = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
             copyArr[i] = arr[i];
         }
         return copyArr;
     }
-    public static String[] copyStirngArr(String[] arr){
-        String [] copyArr = new String[arr.length];
-        for (int i = 0; i < arr.length; i++){
+
+    public static double[] copyDoubleArr(double[] arr) {
+        double[] copyArr = new double[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            copyArr[i] = arr[i];
+        }
+        return copyArr;
+    }
+
+    public static String[] copyStirngArr(String[] arr) {
+        String[] copyArr = new String[arr.length];
+        for (int i = 0; i < arr.length; i++) {
             copyArr[i] = arr[i];
         }
         return copyArr;
     }
 
     //CHECK
-    public static boolean checkSort(int [] arr){
-        for (int i = 0; i < arr.length-1; i++){
-            if (i > i+1){
+    public static boolean checkSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (i > i + 1) {
                 return false;
             }
         }
         return true;
     }
-    public static boolean checkSum(int [] before, int [] after){
-        if (before.length != after.length){
+
+    public static boolean checkSort(double[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (i > i + 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean checkAlpha(String[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i].compareTo(arr[i + 1]) > 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean checkSum(int[] before, int[] after) {
+        if (before.length != after.length) {
             return false;
         }
         int sumBef = 0;
         int sumAft = 0;
+        for (int i = 0; i < before.length - 1; i++) {
+            sumBef = sumBef + before[i];
+            sumAft = sumAft + after[i];
+        }
+        if (sumAft != sumAft) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+    public static boolean checkSum(double [] before, double [] after){
+        if (before.length != after.length){
+            return false;
+        }
+        double sumBef = 0;
+        double sumAft = 0;
         for (int i =0; i < before.length-1; i++){
             sumBef = sumBef + before[i];
             sumAft = sumAft + after[i];
@@ -139,5 +182,6 @@ public class InPlaceSorts {
         }
     }
 }
+
 
 
--- src/Runner.java
+++ src/Runner.java
@@ -1,11 +1,84 @@
+import org.omg.PortableInterceptor.INACTIVE;
+
 public class Runner {
 
     public static void main(String[] args) {
 
-        String [] randomStringArray = inPlaceSorts.randomStringArr(10);
-                System.out.println("Before: ");
-        System.out.println(inPlaceSorts.printArray(randomStringArray));
+        System.out.print("INSERTION SORT");
+        System.out.println();
+        int[] testlist = InPlaceSorts.randIntArr(10);
+        int[] oldlist = InPlaceSorts.copyIntArr(testlist);
+        long time = System.nanoTime();
+        for (int num: testlist){
+            System.out.print(num+" ");
+        }
+        System.out.println();
+        InPlaceSorts.insertionSort(testlist);
+        for (int num: testlist){
+            System.out.print(num+" ");
+        }
+        System.out.println();
+        if (InPlaceSorts.checkSort(testlist) && InPlaceSorts.checkSum(oldlist,testlist)){
+            System.out.print("Insertion Sort was successful!");
+        }
+        System.out.println();
+        time = System.nanoTime()-time;
+        System.out.println("Insert Sort ints Time taken: " + time);
+        //for(int cur : testlist){
+        //  System.out.println(cur);
+        //}
+        System.out.print("-----------------------------------------------------");
+        System.out.println();
 
 
+        System.out.print("BUBBLE SORT");
+        System.out.println();
+        String [] StringList = InPlaceSorts.randomStringArr(3,1);
+        String [] oldStringList = InPlaceSorts.copyStirngArr(StringList);
+        long time2 = System.nanoTime();
+        for (String num: StringList){
+            System.out.print(num+" ");
+        }
+        System.out.println();
+        InPlaceSorts.bubbleSort(StringList);
+        for (String num: StringList){
+            System.out.print(num+" ");
+        }
+        System.out.println();
+        if (InPlaceSorts.checkAlpha(StringList)){
+            System.out.print("Bubble Sort was successful!");
+        }
+        time2 = System.nanoTime() - time2;
+        System.out.println("\nBubble Sort Strings Time taken: " + time2);
+        //for(String cur : randomWords){
+        //    System.out.println(cur);
+        //}
+        System.out.print("-----------------------------------------------------");
+        System.out.println();
+
+
+        System.out.print("SELECTION SORT");
+        System.out.println();
+        double[] testDouble = InPlaceSorts.randDoubleArr(10);
+        double [] oldList = InPlaceSorts.copyDoubleArr(testDouble);
+        long time3 = System.nanoTime();
+        for (double num: testDouble){
+            System.out.print(num+" ");
+        }
+        System.out.println();
+        InPlaceSorts.selectionSort(testDouble);
+        for (double num: testDouble){
+            System.out.print(num+" ");
+        }
+        System.out.println();
+        if (InPlaceSorts.checkSort(testDouble) && InPlaceSorts.checkSum(oldList,testDouble)){
+            System.out.print("Selection Sort was successful!");
+        }
+        System.out.println();
+        time = System.nanoTime()-time;
+        System.out.println("Selection Sort ints Time taken: " + time);
+        //for(int cur : testlist){
+        //  System.out.println(cur);
+        //}
     }
 }
