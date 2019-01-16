e59e9884f4afd52291aba31e66a9aeae71d30382
BT_1N3_28 <yibarra0090#bths.edu>
2019-01-16 14:43:52 -0500

    Binary Search added!

--- src/InPlaceSorts.java
+++ src/InPlaceSorts.java
@@ -181,6 +181,55 @@ public class InPlaceSorts {
             return true;
         }
     }
+    public static int binarySearch(int[] elements, int target){
+        int left = 0;
+        int right = elements.length - 1;
+        while (left <= right){
+            int middle = (left + right) /2;
+            if (target < elements[middle]){
+                right = middle - 1;
+            }
+            else if (target > elements[middle]){
+                left = middle + 1;
+            }
+            else {
+                return middle;
+            }
+        }
+        return -1;
+    }
+    public static double binarySearch(double[] elements, int target){
+        int left = 0;
+        int right = elements.length - 1;
+        while (left <= right){
+            int middle = (left + right) /2;
+            if (target < elements[middle]){
+                right = middle - 1;
+            }
+            else if (target > elements[middle]){
+                left = middle + 1;
+            }
+            else {
+                return middle;
+            }
+        }
+        return -1;
+    }
+    public static int binarySearch(String[] elements, String target) {
+        int left = 0;
+        int right = elements.length - 1;
+        while (left <= right) {
+            int middle = (left + right) / 2;
+            if (elements[middle].compareTo(target) < 0) {
+                left = middle + 1;
+            } else if (elements[middle].compareTo(target) > 0) {
+                right = middle - 1;
+            } else {
+                return middle;
+            }
+        }
+        return -1;
+    }
 }
 
 
