a34c2990c44243a4a0ff2f05526ae74f9821a8e7
BT_1N3_28 <yibarra0090@bths.edu>
2019-02-01 14:39:16 -0500

    Merge Sort added!

--- src/InPlaceSorts.java
+++ src/InPlaceSorts.java
@@ -230,6 +230,48 @@ public class InPlaceSorts {
         }
         return -1;
     }
+    public static void merge(int[] elements, int from, int mid, int to, int[] temp){
+        int i = from;
+        int j = mid + 1;
+        int k = from;
+        while (i <= mid && j <= to){
+            if (elements[i] < elements[j]){
+                temp[k] = elements[i];
+                i++;
+            }
+            else{
+                temp[k] = elements[j];
+                j++;
+            }
+            k++;
+        }
+        while (i <= mid){
+            temp[k] = elements[i];
+            i++;
+            k++;
+        }
+        while (j <= to){
+            temp[k] = elements[j];
+            j++;
+            k++;
+        }
+        for (k = from; k <= to; k++){
+            elements[k] = temp[k];
+        }
+    }
+    public void mergeSortHelper(int[] arr, int left, int right, int[] temp){
+        if (left < right){
+            int mid = (left+right)/2;
+            mergeSortHelper(arr,left, mid,temp);
+            mergeSortHelper(arr,mid+1,right,temp);
+            merge(arr, left, mid, right, temp);
+        }
+    }
+    public void mergeSort(int[] arr){
+        int n = arr.length;
+        int[] temp = new int[n];
+        mergeSortHelper(arr,0,n-1,temp);
+    }
 }
 
 
